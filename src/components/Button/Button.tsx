e3fefae07f946d35d75a03e53b2f6be2423a6416
dariarus <rusanova_dv@mail.ru>
2024-02-16 19:48:29 +0000

    fix: установить библиотеку classnames и применить ее вместо дублирования стилей с разными пропсами

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,21 @@
 import React, { type FC } from 'react'
+import * as classNames from 'classnames'
 
 import styles from './Button.module.css'
 
-type Props = {
+interface Props {
   name: string
   isWithBackground: boolean
 }
 
 export const Button: FC<Props> = (props) => {
+  const buttonStyles = classNames(styles.button as string, {
+    [styles['button_background_with-image']]: props.isWithBackground,
+    [styles.button_background_common]: !props.isWithBackground
+  })
+
   return (
-    <button className={props.isWithBackground
-      ? `${styles.button} ${styles['button_background_with-image']}`
-      : `${styles.button} ${styles.button_background_common}`}>
+    <button className={buttonStyles}>
       {props.name}
     </button>
   )
