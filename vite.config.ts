0fd5b6199979ca87f496b3140c70b35ee84a5825
dariarus <rusanova_dv@mail.ru>
2024-02-21 22:02:43 +0300

    fix: переместить index.html в папку src. Поменять конфигурацию vite.config.ts в связи с перемещением файла

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react(), eslint()]
+  plugins: [react(), eslint()],
+  root: './src',
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  }
 })
