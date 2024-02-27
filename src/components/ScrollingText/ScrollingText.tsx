08a726740cd9eadfc8843e8d1a5b9f7662793643
dariarus <rusanova_dv@mail.ru>
2024-02-27 19:32:27 +0300

    fix: настроить бесконечную прокрутку ленты новостей

--- src/components/ScrollingText/ScrollingText.tsx
+++ src/components/ScrollingText/ScrollingText.tsx
@@ -4,25 +4,43 @@ import styles from './ScrollingText.module.css';
 
 export const ScrollingText: FC = () => {
   const tmpNews = [
+    'FIRST0xv8...12tk claimed free 1-week period',
     '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
-    '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    // '0xv8...12tk claimed free 1-week period',
+    'FIN0xv8...12tk claimed free 1-week period',
   ]
 
   return (
     <div className={styles['tape']}>
-      <div className={styles['text-container']}>
-        {
-          tmpNews.map(news => (
-            <div className={styles['text']}>{news}</div>
-          ))
-        }
+      <div className={styles['list-container']}>
+        <ul className={styles['list']}>
+          {
+            tmpNews.map(news => (
+              <li className={styles['list-item']}>{news}</li>
+            ))
+          }
+        </ul>
+        <ul className={styles['list']}>
+          {
+            tmpNews.map(news => (
+              <li className={styles['list-item']}>{news}</li>
+            ))
+          }
+        </ul>
+        <ul className={styles['list']}>
+          {
+            tmpNews.map(news => (
+              <li className={styles['list-item']}>{news}</li>
+            ))
+          }
+        </ul>
       </div>
     </div>
   )
