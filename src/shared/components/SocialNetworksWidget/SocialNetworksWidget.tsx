f4ddc1217124b46de829aed0caedc8dcb70a0d76
dariarus <rusanova_dv@mail.ru>
2024-03-11 16:03:49 +0300

    fix: заменить иконки соц.сетей в формате svg на исходный формат png. Убрать импорт иконок в jsx, заменить его модификаторами в css

--- src/shared/components/SocialNetworksWidget/SocialNetworksWidget.tsx
+++ src/shared/components/SocialNetworksWidget/SocialNetworksWidget.tsx
@@ -1,27 +1,30 @@
-import React, { type FC } from 'react';
+import React, { type FC } from "react";
 
-import instagramIcon from '/images/instagram-icon.svg';
-import telegramIcon from '/images/telegram-icon.svg';
-import linkedinIcon from '/images/linkedin-icon.svg';
-import facebookIcon from '/images/facebook-icon.svg';
-
-import { Link } from '../Link/Link';
-import styles from './SocialNetworksWidget.module.css';
+import { Link } from "../Link/Link";
+import styles from "./SocialNetworksWidget.module.css";
 
 export const SocialNetworksWidget: FC = () => {
   return (
-    <div className={styles['widget']}>
+    <div className={styles["widget"]}>
       <Link href="/example/change-me" modifier="image" title="Instagram">
-        <span className={styles['widget__icon']} style={{ backgroundImage: `url(${instagramIcon})` }}></span>
+        <span
+          className={`${styles["widget__icon"]} ${styles["widget__icon_instagram"]}`}
+        ></span>
       </Link>
       <Link href="/example/change-me" modifier="image" title="Telegram">
-        <span className={styles['widget__icon']} style={{ backgroundImage: `url(${telegramIcon})` }}></span>
+        <span
+          className={`${styles["widget__icon"]} ${styles["widget__icon_telegram"]}`}
+        ></span>
       </Link>
       <Link href="/example/change-me" modifier="image" title="Linkedin">
-        <span className={styles['widget__icon']} style={{ backgroundImage: `url(${linkedinIcon})` }}></span>
+        <span
+          className={`${styles["widget__icon"]} ${styles["widget__icon_linkedin"]}`}
+        ></span>
       </Link>
       <Link href="/example/change-me" modifier="image" title="Facebook">
-        <span className={styles['widget__icon']} style={{ backgroundImage: `url(${facebookIcon})` }}></span>
+        <span
+          className={`${styles["widget__icon"]} ${styles["widget__icon_facebook"]}`}
+        ></span>
       </Link>
     </div>
   );
