e2ccc4ec61fb57e6ac22f7840ec3f236467fa6e3
dariarus <rusanova_dv@mail.ru>
2024-03-16 17:52:41 +0300

    fix: доработать положение контента на странице Private Presales

--- src/components/PrivatePresaleProfile/PrivatePresaleProfile.tsx
+++ src/components/PrivatePresaleProfile/PrivatePresaleProfile.tsx
@@ -3,6 +3,7 @@ import React, {FC} from 'react';
 import styles from './PrivatePresaleProfile.module.css'
 // import {Button} from '../button/button';
 import {StatusRectangle} from '../StatusRectangle/StatusRectangle';
+import {Button} from '../button/button';
 
 type Props = {
   privateSalesStatus: 'available' | 'soon' | 'finished';
@@ -21,41 +22,37 @@ export const PrivatePresaleProfile: FC<Props> = ({privateSalesStatus}) => {
             </h3>
             <StatusRectangle title="5d 13h 12min 43sec" status="active"/>
           </div>
-          <p className={`${styles['text']} ${styles['text_mod']}`}>Choose the amount of tokens you want to buy and make a payment in any token.</p>
-          <div>
-            <div className={styles['coast-info']}>
+          <div className={styles['text-wrap']}>
+            <p className={`${styles['text']}`}>Choose the amount of tokens you want to buy and make a payment in any
+              token.</p>
+          </div>
+          <div className={styles['coast-info']}>
+            <div className={styles['coast-item']}>
               <p className={styles['text']}>Your balance</p>
-              <span className={styles['coast-info__decor']}></span>
+              <span className={styles['coast-item__decor']}></span>
               <p className={styles['text']}>1 ETH ($1700)</p>
             </div>
-            <div className={styles['coast-info']}>
-              <p className={styles['text']}>Your balance</p>
-              <span className={styles['coast-info__decor']}></span>
-              <p className={styles['text']}>1 ETH ($1700)</p>
+            <div className={styles['coast-item']}>
+              <p className={styles['text']}>Presale Price</p>
+              <span className={styles['coast-item__decor']}></span>
+              <p className={styles['text']}>0.2 ETH ($340)</p>
             </div>
-            <div className={styles['coast-info']}>
+            <div className={styles['coast-item']}>
               <p className={styles['text']}>Amount of NFT</p>
-              <span className={styles['coast-info__decor']}></span>
+              <span className={styles['coast-item__decor']}></span>
               <p className={styles['text']}>2/5</p>
             </div>
-            <div className={styles['coast-info']}>
-              <p className={styles['text']}>Your balance</p>
-              <span className={styles['coast-info__decor']}></span>
-              <p className={styles['text']}>1 ETH ($1700)</p>
+            <div className={styles['coast-item']}>
+              <p className={styles['text']}>Total coast</p>
+              <span className={styles['coast-item__decor']}></span>
+              <p className={styles['text']}>0.2 ETH ($340)</p>
             </div>
+          </div>
+          <div className={styles['button-wrap']}>
+            <Button name="Mint 2 NFT" isWithBackground={false}/>
           </div>
         </>
       }
-      {/*<h3 className={styles['heading']}>*/}
-      {/*  ? "Be an early bird!"*/}
-      {/*  : "You’re not whitelisted yet :("}</h3>*/}
-      {/*<p className={styles['text']}>{props.isUserActive*/}
-      {/*  ? "Your wallet has been whitelisted! Claim your NFT now!"*/}
-      {/*  : "To participate in AirDrop please send us an information about your project"}</p>*/}
-      {/*<Button name={props.isUserActive*/}
-      {/*  ? "Claim NFT"*/}
-      {/*  : "Get whitelisted"}*/}
-      {/*        isWithBackground={false}/>*/}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './style.css';
-import './fonts/fonts.css';
-import './vendor/normalize.css';
+import "./style.css";
+import "./fonts/fonts.css";
+import "./vendor/normalize.css";
 
-import App from './components/app/app';
+import App from "./components/app/app";
 
 const router = createBrowserRouter([
   {
     path: "/*",
-    element: <App/>,
-  }
+    element: <App />,
+  },
 ]);
 
-const app = ReactDOM.createRoot(
-  document.getElementById('app') as HTMLElement
-);
+const app = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 app.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
