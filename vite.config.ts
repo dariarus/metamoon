796db779df5987f142568b137c67e9d8d35caefb
dariarus <rusanova_dv@mail.ru>
2024-03-18 16:55:17 +0300

    fix: настроить конфигурацию vite, чтобы проект билдился в корень

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import eslint from 'vite-plugin-eslint';
-import stylelint from 'vite-plugin-stylelint';
-import prettier from 'vite-plugin-prettier';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
+import stylelint from "vite-plugin-stylelint";
+import prettier from "vite-plugin-prettier";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
@@ -15,12 +15,12 @@ export default defineConfig({
     prettier(),
     tsconfigPaths(),
   ],
-  root: './src',
-  publicDir: '../public',
+  root: "./src",
+  publicDir: "../public",
   build: {
     rollupOptions: {
-      input: '/index.html',
+      input: "/index.html",
     },
-    outDir: './dist',
+    outDir: "../dist",
   },
 });
